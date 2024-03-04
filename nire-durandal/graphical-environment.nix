291ca06b975ff1f48ad1642ecb5f0cf40605a248
Nire Bryce <nire@computernope.net>
2024-03-03 23:55:26 -0500

    gnome settings stick through kde

--- nire-durandal/graphical-environment.nix
+++ nire-durandal/graphical-environment.nix
@@ -8,4 +8,7 @@
   #TODO: variablize so it's a choice of DM and WM
   services.xserver.displayManager.sddm.wayland.enable = true;
   services.xserver.desktopManager.plasma6.enable = true;
+
+# make GTK apps obey theme settings
+ programs.dconf.enable = true;
 }
