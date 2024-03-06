c632953d831c63722b4cb78d9d7b466eba7577eb
= <=>
2024-03-06 01:46:51 -0500

    ugh

--- _specialized/_bugfixes/_suspend/_gpp0-etc.nix
+++ _specialized/_bugfixes/_suspend/_gpp0-etc.nix
@@ -4,24 +4,25 @@
 {
   environment.systemPackages = with pkgs; [ zenstates ];
   # - https://forum.manjaro.org/t/system-do-not-wake-up-after-suspend/76681/2
-  fix-gpp = pkgs.writeScriptBin "_bugfix-suspend-gpp" ''
-       #!${pkgs.bash}/bin/bash
-       ${pkgs.sh}/bin/sh -c "echo GPP8 > /proc/acpi/wakeup && echo gpp8 > /proc/acpi/wakeup"  ; # might need to be single caret
-     '';
-  
-  
-    # from zenstates code comments
-    before-sleep = pkgs.writeScriptBin "before-sleep" ''
-       #!${pkgs.bash}/bin/bash
-       ${pkgs.zenstates}/bin/zenstates --c6-disable
-    '';
+  # fix-gpp = pkgs.writeScriptBin "_bugfix-suspend-gpp" ''
+  #      #!${pkgs.bash}/bin/bash
+  #      ${pkgs.sh}/bin/sh -c "echo GPP8 > /proc/acpi/wakeup && echo gpp8 > /proc/acpi/wakeup"  ; # might need to be single caret
+  #    '';
+  # 
+  # 
+  #   # from zenstates code comments
+  #   before-sleep = pkgs.writeScriptBin "before-sleep" ''
+  #      #!${pkgs.bash}/bin/bash
+  #      ${pkgs.zenstates}/bin/zenstates --c6-disable
+  #   '';
   
   systemd.services.before-sleep = {
       description = "_BUGFIX-suspend (Ryzen disable c6 suspend)";
       wantedBy = [ "sleep.target" "hibernate.target" ];
       before = [ "sleep.target" ];
       serviceConfig.Type = "oneshot";
-      serviceConfig.ExecStart="${before-sleep}";
+      # serviceConfig.ExecStart="${before-sleep}";
+      serviceConfig.ExecStart="zenstates --c6-disable";
   };
 
 # Gigabyte b550 sleep bug
@@ -42,6 +43,7 @@
       wantedBy = [ "multi-user.target" ];
       serviceConfig.Type = "oneshot";
       serviceConfig.ExecStart = "${_bugfix-suspend-gpp}";
+      serviceConfig.ExecStart = "echo GPP8 > /proc/acpi/wakeup && echo gpp8 > /proc/acpi/wakeup";
   };
 
-  }
+}
