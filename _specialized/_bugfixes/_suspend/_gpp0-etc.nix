073f40921992dd7d169cc534f10e53b910cacdd3
= <=>
2024-03-06 01:43:30 -0500

    ugh

--- _specialized/_bugfixes/_suspend/_gpp0-etc.nix
+++ _specialized/_bugfixes/_suspend/_gpp0-etc.nix
@@ -9,30 +9,30 @@
       wantedBy = [ "sleep.target" "hibernate.target" ];
       before = [ "sleep.target" ];
       serviceConfig.Type = "oneshot";
-      serviceConfig.ExecStart="zenstates --c6-disable";
+      serviceConfig.ExecStart="${before-sleep}";
   };
 
 # Gigabyte b550 sleep bug
   # https://www.reddit.com/r/gigabyte/comments/p5ewjn/b550i_pro_ax_f13_bios_sleep_issue_on_linux/
   # 
   # I have a temporary fix for now. You have to disable GPP0 wakeup which is a GPP bridge to the NVMe drive in M.2 slot. Check your wakeup table using cat /proc/acpi/wakeup and look at GPP0. It should say *enabled. Using sudo /bin/sh -c '/bin/echo GPP0 > /proc/acpi/wakeup' you can set it to *disabled. PC should suspend normally then. 
-  systemd.services.fix-suspend-gpp0 = {
-      description = "_BUGFIX-suspend (GPP0)";
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig.Type = "oneshot";
-      serviceConfig.ExecStart ="echo GPP0 >> /proc/acpi/wakeup";
-  };
-  
+  # systemd.services.fix-suspend-gpp0 = {
+  #     description = "_BUGFIX-suspend (GPP0)";
+  #     wantedBy = [ "multi-user.target" ];
+  #     serviceConfig.Type = "oneshot";
+  #     serviceConfig.ExecStart ="echo GPP0 >> /proc/acpi/wakeup";
+  # };
+  # 
   # https://www.reddit.com/r/archlinux/comments/11urtla/systemctl_suspend_hibernate_and_hybridsleep_all/
   # gpp8 too.  potentially PXTH
-  systemd.services.fix-suspend-gpp8 = {
-      description = "_BUGFIX-suspend (GPP8)";
+  systemd.services.fix-suspend-gpp0-gpp8 = {
+      description = "_BUGFIX-suspend (GPP0, GPP8)";
       wantedBy = [ "multi-user.target" ];
       serviceConfig.Type = "oneshot";
-      serviceConfig.ExecStart = "echo GPP8 >> /proc/acpi/wakeup";
+      serviceConfig.ExecStart = "${_bugfix-suspend-gpp}";
   };
 
-  _bugfix-suspend-gpp0-gpp8 = pkgs.writeScriptBin "_bugfix-suspend-gpp0-gpp8" ''
+  fix-gpp = pkgs.writeScriptBin "_bugfix-suspend-gpp" ''
      #!${pkgs.bash}/bin/bash
      ${pkgs.sh}/bin/sh -c "echo GPP8 > /proc/acpi/wakeup && echo gpp8 > /proc/acpi/wakeup"  ; # might need to be single caret
    '';
