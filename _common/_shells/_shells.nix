c3816d9fed48846b9fef68d8d30ea3c3a668b2c4
Nire Bryce <nire@computernope.net>
2024-03-13 15:14:39 -0400

    console font

--- _common/_shells/_shells.nix
+++ _common/_shells/_shells.nix
@@ -1,5 +1,6 @@
 { pkgs, ...}:
 {
+  # console.font = "FiraCode";
   # programs._.enable = true;
   programs.zsh.enable = true;
   programs.bash.enable = true;
@@ -15,6 +16,7 @@
   console = {
     keyMap = "us";
     # useXkbConfig = true; # use xkb.options in tty.
+    console.font = "FiraCode Nerd Font Mono";
   };
 
 users.users.elly.shell = pkgs.zsh;
