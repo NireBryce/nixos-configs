feadcc1ae98c0efe986d7250422aeded1d67745f
Nire Bryce <nire@computernope.net>
2024-03-13 15:42:23 -0400

    lid sensor fix for broken one on my one mix 3

--- nire-lysithea/default.nix
+++ nire-lysithea/default.nix
@@ -21,8 +21,10 @@ in
       ../_specialized/_sound
       #TODO: ../_specialized/_gpu/_intel.nix
       #TODO: ../_specialized/_mouse/_one-mix-3-nub.nix
-      #TODO: ../_bugfixes/_suspend/_one-mix-3-lid-sensor-wake-disable.nix
       #TODO: ../_bugfixes/_suspend/_one-mix-3-sleep-crash.nix # suspect similar to b550
+      
+    # fixes
+      ../_bugfixes/_suspend/_one-mix-3-lid-sensor.nix
   ];
   
   
