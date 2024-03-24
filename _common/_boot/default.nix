b6f8e3f5a7f2dd34fc03face2896d3a714ece8fa
Nire Bryce <nire@computernope.net>
2024-03-23 21:39:21 -0400

    changed _boot/default.nix to have more consistent comments

--- _common/_boot/default.nix
+++ _common/_boot/default.nix
@@ -1,23 +1,21 @@
 # This is where the boot options live, along with other things that need to be set near then
-{ ... }:
 {
   imports = [ 
     ./_delete-root.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-  networking.networkmanager.enable = true;  # Needs to be 'true' for KDE networking
-  nixpkgs.config.allowUnfree = true;
-  
-  # Set time zone.
-  time.timeZone = "America/New_York";
-  
+    boot.loader.systemd-boot.enable = true;
+    boot.loader.efi.canTouchEfiVariables = true;
+  # nix settings 
+    nix.settings.experimental-features = [ "nix-command" "flakes" ];
+    nixpkgs.config.allowUnfree = true;
+  # Wifi
+    networking.networkmanager.enable = true;  # Needs to be 'true' for KDE networking
+  # time zone.
+    time.timeZone = "America/New_York";  
   # i18n
-  i18n.defaultLocale = "en_US.UTF-8";
+    i18n.defaultLocale = "en_US.UTF-8";
 }
 
 # Monument for fallen useful code
