23408ecf4998c30b29a5e1a41da0d28d08f0cfd0
Nire Bryce <nire@computernope.net>
2024-05-11 23:40:00 -0400

    ??? I just lost the ability to use home-manager

--- home-manager/_common/_utils/_home-manager.nix
+++ home-manager/_common/_utils/_home-manager.nix
@@ -7,6 +7,6 @@
     };
   };
   home.stateVersion = "22.11"; # To figure this out (in-case it changes) you can comment out the line and see what version it expected.
-  # programs.home-manager.enable = true;
+  programs.home-manager.enable = true;
 
 }
