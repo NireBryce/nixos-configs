839b74e4a62f1034ce6d44e9f6d8381c8ffe0a0c
Nire Bryce <nire@computernope.net>
2024-05-24 04:07:09 +0000

    May 24, 2024, 12:07 AM

--- home-manager/_sys.session-variables.nix
+++ home-manager/_sys.session-variables.nix
@@ -9,5 +9,17 @@
     PAGER="less -R";
     MANPAGER="bat --language man";
     LC_CTYPE="en_US.UTF-8";
+    LS_COLORS="$(vivid generate dracula)";          # https://github.com/sharkdp/vivid
+    EZA_COLORS="$(vivid generate dracula)";         
+
   };
 }
+
+
+# # For LS_COLORS customization options run this in shell:
+# for theme in $(vivid themes); do
+#     echo "Theme: $theme"
+#     LS_COLORS=$(vivid generate $theme)
+#     ls
+#     echo
+# done
