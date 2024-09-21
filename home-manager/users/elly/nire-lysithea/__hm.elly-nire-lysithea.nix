119e2334562838c108765836f339763a0e0ff6d2
Nire Bryce <nire@computernope.net>
2024-09-21 17:14:19 -0400

    consolodated things into the various user configs

--- home-manager/users/elly/nire-lysithea/__hm.elly-nire-lysithea.nix
+++ home-manager/users/elly/nire-lysithea/__hm.elly-nire-lysithea.nix
@@ -2,21 +2,39 @@
 	self,
 	... 
 }:
-let flakePath = self;
+let 
+  flakePath = self;
+in let
+  ## imports
 
+  #* home-manager
+    hm-state                    = "${flakePath}/home-manager/meta/_hm.stateVersion.nix";
+    hm-nix-defaults             = "${flakePath}/home-manager/_hm.hm-nix-defaults.nix";  
+  #* user.elly
+    elly-cli-packages           = "${flakePath}/home-manager/users/elly/_hm.elly.pkgs.general.nix";
+    elly-darwin-packages        = "${flakePath}/home-manager/users/elly/_hm.elly.pkgs.darwin.nix";            
+    elly-development-packages   = "${flakePath}/home-manager/dev/default.nix";
+    elly-dotfiles               = "${flakePath}/home-manager/dotfiles/default.nix";
+    elly-git-hm                 = "${flakePath}/home-manager/users/elly/_hm.elly.git.nix";
+    elly-shells                 = "${flakePath}/home-manager/_hm.shells.nix";
 in {
-  imports = [
-    "${flakePath}/home-manager/users/elly/_hm.elly.pkgs.general.nix"            # CLI packages
-    "${flakePath}/home-manager/users/elly/_hm.elly.pkgs.darwin.nix"            # CLI packages
-    
-    "${flakePath}/home-manager/meta/_hm.stateVersion.nix"
-    
-    "${flakePath}/home-manager/home.nix"
-    "${flakePath}/home-manager/_hm.hm-nix-defaults.nix"
-    "${flakePath}/home-manager/users/elly/_hm.elly.git.nix"
+    imports = [
+      hm-state
+      hm-nix-defaults
+      elly-git-hm
+      elly-shells
+      elly-cli-packages
+      elly-darwin-packages
+      elly-development-packages
+      elly-dotfiles
+      
 
-    
-  ];
+      # Previously on home.nix:
+        
+
+        
+        "${flakePath}/home-manager/_hm.bash.nix"                # bash
+    ];
 
   home.username = "elly";
   home.homeDirectory = "/Users/elly";
