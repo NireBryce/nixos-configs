917cd1278360b82fbd87224575682127c01afe0b
Nire Bryce <nire@computernope.net>
2024-09-23 01:09:38 -0400

    todo: figure out why I did this way back

--- ___overlays/default.nix
+++ ___overlays/default.nix
@@ -1,6 +1,7 @@
 # working from https://github.com/Misterio77/nix-starter-configs/blob/main/standard/overlays/default.nix
 { inputs, ...}:
 {
+  # TODO: no idea where i was going with this
   stable-packages = final: _prev: {
     stable = import inputs.nixpkgs-stable {
       system = final.system;
