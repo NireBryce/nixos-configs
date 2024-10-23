6c3ba47c982bb14a43320082f3afaf67013d0231
Nire Bryce <nire@computernope.net>
2024-10-23 19:12:48 -0400

    steamtinkerlaunch compatability fix alias

--- system-config/nire-durandal-configuration.nix
+++ system-config/nire-durandal-configuration.nix
@@ -268,8 +268,13 @@ in {
         remotePlay.openFirewall      = true;            # Open ports in the firewall for Steam Remote Play
         dedicatedServer.openFirewall = true;            # Open ports in the firewall for Source Dedicated Server
         gamescopeSession.enable      = true;            # third party gamescope compositor
-    }; 
-
+    };
+  #* fix steamtinkerlaunch compatability tool
+    environment.shellAliases = {
+        # https://gist.github.com/jakehamilton/632edeb9d170a2aedc9984a0363523d3
+        steamtinkerlaunch-compataddfix = "mkdir -p $STEAM_EXTRA_COMPAT_TOOL_PATHS/SteamTinkerLaunch && ln -s /run/current-system/sw/bin/steamtinkerlaunch $STEAM_EXTRA_COMPAT_TOOL_PATHS/SteamTinkerLaunch/steamtinkerlaunch"; 
+    };
+    
     xdg.portal = { # fix steam/proton/wine issues with xdg-open https://github.com/NixOS/nixpkgs/issues/160923 
       enable = true;
       xdgOpenUsePortal = true;
