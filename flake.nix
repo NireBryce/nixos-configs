5eb14e73887003cc14db44b13236e337b284fe8f
Nire Bryce <nire@computernope.net>
2024-10-23 20:51:10 -0400

    further refining

--- flake.nix
+++ flake.nix
@@ -4,9 +4,9 @@
     nixpkgs-stable.url                          = "github:NixOS/nixpkgs/nixos-23.11";
   # Unstable
     # nixpkgs.url                                 = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url                                 = "github:NixOS/nixpkgs/nixpkgs-unstable"; # TODO: this is a YOLO fix for https://github.com/nix-community/home-manager/issues/5991,  I have already spent too much time on this
+                                                                                           #* notice that it's nixpkgs.unstable not nixos.unstable 
 
-    # TODO: this is a YOLO fix for https://github.com/nix-community/home-manager/issues/5991,  I have already spent too much time on this
-    nixpkgs.url =                               "github:NixOS/nixpkgs/nixpkgs-unstable"; 
   # Darwin
     darwin.url                                  = "github:LnL7/nix-darwin";
     darwin.inputs.nixpkgs.follows               = "nixpkgs";
