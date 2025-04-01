3ee3f4c76c3572dd2d0d2bfede478e071f670a23
Nire Bryce <nire@computernope.net>
2025-04-01 15:17:41 -0400

    YUBIKEY LOCK ON REMOVE WORKS

--- system-config/common/secrets/yubikey.nix
+++ system-config/common/secrets/yubikey.nix
@@ -22,17 +22,17 @@ in
       # Yubikey 4
       ##
 
-      # Lock the device if you remove the yubikey (use udevadm monitor -p to debug)
-      # #ENV{ID_MODEL_ID}=="0407", # This doesn't match all the newer keys
       # FIXME(yubikey): We only want this to happen if we're undocked, so we need to see how that works. We probably need to run a
       # script that does smarter checks
-      ACTION=="remove",\
-       ENV{ID_BUS}=="usb",\
-       ENV{ID_VENDOR_ID}=="1050",\
-       ENV{ID_VENDOR}=="Yubico",\
-       RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+      # Lock the device if you remove the yubikey (use udevadm monitor -p to debug)
+    #   ACTION=="remove",\
+    #    ENV{ID_MODEL_ID}=="0406",\ # This doesn't match all the newer keys
+    #    ENV{ID_BUS}=="usb",\
+    #    ENV{ID_VENDOR_ID}=="1050",\
+    #    ENV{ID_VENDOR}=="Yubico",\
+    #    RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
 
-      ##
+    #   ##
       # Yubikey 5 BIO
       #
       # NOTE: The remove event for the bio doesn't include the ID_VENDOR_ID for some reason, but we can use the
@@ -40,17 +40,19 @@ in
       # match on "Yubico YubiKey"
       ##
 
-      SUBSYSTEM=="hid",\
-       ACTION=="remove",\
-       ENV{HID_NAME}=="Yubico YubiKey FIDO",\
-       RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
+        SUBSYSTEM=="hid",\
+          ACTION=="remove",\
+          ENV{HID_NAME}=="Yubico YubiKey FIDO+CCID",\
+          RUN+="${pkgs.systemd}/bin/loginctl lock-sessions"
 
-      # FIXME(yubikey): Change this so it only wakes up the screen to the login screen, xset cmd doesn't work
-      # SUBSYSTEM=="hid",\
-      #  ACTION=="add",\
-      #  ENV{HID_NAME}=="Yubico YubiKey FIDO",\
-      #  RUN+="${pkgs.systemd}/bin/loginctl activate 1"
-      #  #RUN+="${lib.getBin pkgs.xorg.xset}/bin/xset dpms force on"
+        # FIXME(yubikey): Change this so it only wakes up th    e screen to the login screen, xset cmd doesn't work
+        SUBSYSTEM=="hid",\
+          ACTION=="add",\
+          ENV{HID_NAME}=="Yubico YubiKey FIDO+CCID",\
+          RUN+="${pkgs.systemd}/bin/loginctl activate 1"
+          
+          #! WARN: testing purposes only until I find how to bind to yubikey unique key
+          # RUN+="${pkgs.systemd}/bin/loginctl unlock-sessions"
     '';
 
 
