72e997ae0437b1aafe0542932b9d4c14ab93c8d2
Nire Bryce <nire@computernope.net>
2025-04-03 15:13:43 -0400

    started skeleton for bootstrap iso

---
+++ system-config/iso.nix
@@ -0,0 +1,17 @@
+# originally https://github.com/EmergentMind/nix-config/blob/f8aad6648cf6e881ab09d16a400af973cb235b4e/hosts/nixos/iso/default.nix
+{ 
+  config,
+  inputs, 
+  lib, 
+  pkgs, 
+  ...
+}:
+{
+    
+  
+
+
+
+
+
+}
