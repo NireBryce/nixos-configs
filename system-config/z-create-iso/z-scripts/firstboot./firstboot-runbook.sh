2fc9f69ec9b0fbc01e9f7aedde4f87b88f8e26d0
Nire Bryce <nire@computernope.net>
2025-04-04 14:22:28 -0400

    legion go start config and disk bootstrap proto

---
+++ system-config/z-create-iso/z-scripts/firstboot./firstboot-runbook.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+set -euo pipefail; shopt -s inherit_set
+
+
+sudo nixos-rebuild boot
+
+cryptsetup open /dev/nvme0n1p7 enc
+sudo mkdir /mnt; 
+sudo mount -o subvol=/ /dev/vmapper/enc /mnt;
+./fs-diff.sh
+
+
+
---
+++ system-config/z-second-boot/disko.nix
@@ -0,0 +1,88 @@
+# originally https://github.com/EmergentMind/nix-config/blob/f8aad6648cf6e881ab09d16a400af973cb235b4e/hosts/common/disks/btrfs-disk.nix#L10
+# NOTE: ... is needed because dikso passes diskoFile
+{
+  lib,
+  disk ? "/dev/nvme0n1",
+  swapSize,
+  ...
+}:
+let
+  _swapSize = "20";
+in
+{
+  disko.devices = {
+    disk = {
+      disk0 = {
+        type = "disk";
+        device = disk;
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              start = "1M";
+              end = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "defaults" ];
+              };
+            };
+            root = {
+              size = "100%";
+              content = {
+                type = "btrfs";
+                extraArgs = [ "-f" ]; # Override existing partition
+                # Subvolumes must set a mountpoint in order to be mounted,
+                # unless their parent is mounted
+                subvolumes = {
+                  "@root" = {
+                    mountpoint = "/";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@home" = {
+                    mountpoint = "/home";
+                    mountOptions = [
+                      "compress=zstd"
+                    ];
+                  };
+                  "@nix" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                   "@persist" = {
+                    mountpoint = "/nix";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@log" = {
+                    mountpoint = "/var/log";
+                    mountOptions = [
+                      "compress=zstd"
+                      "noatime"
+                    ];
+                  };
+                  "@swap" = {
+                    mountpoint = "/.swapvol";
+                    swap.swapfile.size = "${swapSize}G";
+                  };
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
