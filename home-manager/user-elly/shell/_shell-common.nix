b641ee8dc35c2cb4ce67120055da1eec3f45a232
Nire Bryce <nire@computernope.net>
2025-04-04 22:15:19 -0400

    cleaned u0 old abbrs

--- home-manager/user-elly/shell/_shell-common.nix
+++ home-manager/user-elly/shell/_shell-common.nix
@@ -35,29 +35,27 @@
   #    not declarative -- it saves them to abbr even if
   #    you remove them from the zsh config
     programs.zsh.zsh-abbr.abbreviations = {
-        "abbr remove"                = "abbr erase";
-        "abbr rm"                    = "abbr erase";
-        "cs-zsh-bindings"            = "bindkey";
-        "highlighting-theme-default" = "fast-theme sv-orple";
-        "wh"                         = "wormhole";
-        "whence"                     = "type -a";
-        "zsh-keymap"                 = "bindkey";
-        "rebuild-system"             = "nh os switch .";
-        "rebuild-home"               = "nh home switch ."; 
+        "abbr remove"           = "abbr erase";
+        "abbr rm"               = "abbr erase";
+        "cs-zsh-bindings"       = "bindkey";
+        "highlighting"          = "fast-theme sv-orple";
+        "wh"                    = "wormhole";
+        "whence"                = "type -a";
+        "zsh-keymap"            = "bindkey";
     };
 
     home.sessionVariables = { 
-        EDITOR = "micro";
-        MICRO_TRUECOLOR = 1;
-        NIXPKGS_ALLOW_UNFREE = 1;
-        PYTHONBREAKPOINT = "ipdb.set_trace";
-        COLORTERM="truecolor";
-        PAGER="less -R";
-        MANPAGER="${pkgs.bat}/bin/bat --language man";
-        LC_CTYPE="en_US.UTF-8";
-        LS_COLORS="$(${pkgs.vivid}/bin/vivid generate dracula)";  # https://github.com/sharkdp/vivid
-        EZA_COLORS="$(${pkgs.vivid}/bin/vivid generate dracula)";
-        STARSHIP_CONFIG="$HOME/.config/starship.toml";
-        STARSHIP_CACHE="$HOME/.cache/starship";
+        EDITOR                  = "micro";
+        MICRO_TRUECOLOR         = 1;
+        NIXPKGS_ALLOW_UNFREE    = 1;
+        PYTHONBREAKPOINT        = "ipdb.set_trace";
+        COLORTERM               = "truecolor";
+        PAGER                   = "less -R";
+        MANPAGER                = "${pkgs.bat}/bin/bat --language man";
+        LC_CTYPE                = "en_US.UTF-8";
+        LS_COLORS               = "$(${pkgs.vivid}/bin/vivid generate dracula)";  # https://github.com/sharkdp/vivid
+        EZA_COLORS              = "$(${pkgs.vivid}/bin/vivid generate dracula)";
+        STARSHIP_CONFIG         = "$HOME/.config/starship.toml";
+        STARSHIP_CACHE          = "$HOME/.cache/starship";
   };
 }
