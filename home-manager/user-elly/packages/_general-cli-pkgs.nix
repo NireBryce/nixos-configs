92ee1ee9d01d2699804caf488bbbfe5dac907355
Nire Bryce <nire@computernope.net>
2025-05-27 12:19:48 -0400

    removed font from general hm pkgs

--- home-manager/user-elly/packages/_general-cli-pkgs.nix
+++ home-manager/user-elly/packages/_general-cli-pkgs.nix
@@ -6,12 +6,7 @@
 # TODO: make server config
 
 
-  fonts.fontconfig.enable = true;
-
   home.packages = with pkgs; [    # TODO: figure out which should be modularized for, say, headless machines
-      nerd-fonts.fira-code
-      nerd-fonts.iosevka
-      nerd-fonts.jetbrains-mono
 
     ## Keybinds
       
