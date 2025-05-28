259f4dc6a29d70979dcbabc9ffc2e1bad3fa818d
Nire Bryce <nire@computernope.net>
2025-05-28 12:54:06 -0400

    hm pkgs refactor

--- home-manager/user-elly/packages/linux/gui-pkgs/editors-gui.nix
+++
@@ -1,9 +0,0 @@
-{ 
-    pkgs, 
-    ... 
-}: 
-{
-    home.packages = with pkgs;[ 
-        vscode-fhs                    # vscode                                    https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/applications/editors/vscode/
-    ];
-}
