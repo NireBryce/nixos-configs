b55fc9487c454f657019840e8a003b497b3b721e
Nire Bryce <nire@computernope.net>
2025-05-28 14:48:05 -0400

    topgrade

--- home-manager/user-elly/packages/hardware-independent/cli/topgrade/topgrade.nix
+++ home-manager/user-elly/packages/hardware-independent/cli/topgrade/topgrade.nix
@@ -7,7 +7,23 @@
         topgrade                      # upgrade all the things (nix sorta broken) https://github.com/topgrade-rs/topgrade
     ];
     home.file = {
-        "./.config/topgrade/config.toml"     .source = ./config.toml;
+        "./.config/topgrade/config.toml" = { 
+            executable = false; 
+            text = ''
+                [misc]
+                # Disable specific steps - same options as the command line flag
+                disable = [
+                    "nix", 
+                    "uv",
+                    "micro",
+                    "vim",
+                    "vscode",
+
+                    # nixos
+                    "system" 
+                ]
+            ''; 
+        };
     };
 }
 
