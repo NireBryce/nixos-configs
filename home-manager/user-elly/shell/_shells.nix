2dbc81c3c57a10a6cf1f85bd7534ce2b75139616
Nire Bryce <nire@computernope.net>
2025-05-28 20:27:02 -0400

    kitty

--- home-manager/user-elly/shell/_shells.nix
+++ home-manager/user-elly/shell/_shells.nix
@@ -32,18 +32,7 @@ in {
           
 
         
-        kitty = {
-          extraConfig = ''
-            clipboard_control write-clipboard write-primary read-clipboard-ask read-primary-ask
-            kitty_mod ctrl+shift
-
-            map kitty_mod+c copy_to_clipboard
-            map cmd+c       copy_or_interrupt
-
-            map kitty_mod+v paste_from_clipboard
-            map cmd+v       paste_from_clipboard
-          '';
-        };
+        
     };
     # end programs
 
