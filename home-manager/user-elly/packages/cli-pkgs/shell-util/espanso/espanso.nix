e77ac5b5b5508f2f1067464e932b0608e7a08792
Nire Bryce <nire@computernope.net>
2025-05-29 00:57:54 -0400

    espanso

--- home-manager/user-elly/packages/cli-pkgs/shell-util/espanso/espanso.nix
+++ home-manager/user-elly/packages/cli-pkgs/shell-util/espanso/espanso.nix
@@ -4,14 +4,14 @@
 }:
  
 {
-    programs.espanso = {
+    services.espanso = {
         enable  = true;
         waylandSupport = true;
-        configs = [ "./.config/espanso/config/config.yml" ];
     };
-    home.file = {# empty for now
-        "./.config/espanso/config/config.yml" = {
-            source = ./config.yml;
-        };
-    };
+    # home.file = {# empty for now
+    #     "./.config/espanso/config/config.yml" = {
+    #         source = ./config.yml;
+    #     };
+    # };
 }
+
