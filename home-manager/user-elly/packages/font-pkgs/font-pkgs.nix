eb73d288c7de9592d196b5c5783e103a3be920e5
Nire Bryce <nire@computernope.net>
2025-06-12 19:49:17 -0400

    ...

--- home-manager/user-elly/packages/font-pkgs/font-pkgs.nix
+++ home-manager/user-elly/packages/font-pkgs/font-pkgs.nix
@@ -1,7 +1,8 @@
 { pkgs, ...}:
 
 {
-    home.packages = with pkgs; [    # TODO: duplicated in system fonts, please remove
+    # TODO: duplicated in system fonts, is this for non-nix hosts? consider moving to there
+    home.packages = with pkgs; [            
         nerd-fonts.fira-code
         nerd-fonts.iosevka
         nerd-fonts.jetbrains-mono
