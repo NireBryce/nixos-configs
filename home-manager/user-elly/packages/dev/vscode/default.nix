8fdcc32b4a27309df0055876613cb8a8a4702575
Nire Bryce <nire@computernope.net>
2025-06-15 21:22:26 -0400

    added nixd to vscode config

--- home-manager/user-elly/packages/dev/vscode/default.nix
+++ home-manager/user-elly/packages/dev/vscode/default.nix
@@ -5,10 +5,13 @@
  
 {
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
-    
+    environment.systemPackages = with pkgs; [
+        nixd # nix LSP
+    ];
     programs.nix-ld.enable = true;      # Needed for VSCode remote connection, etc
     programs.vscode = {
         enable = true;
         package = pkgs.vscode-fhs;
     };
+    
 }
