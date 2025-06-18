e793d38909bec3d9ca10cb206056f3a3009ea32a
Nire Bryce <nire@computernope.net>
2025-06-18 17:28:13 -0400

    typo fix

--- flake.nix
+++ flake.nix
@@ -47,7 +47,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     inputs.nixpkgs-lib.follows = "nixpkgs";
   # import-tree flake-parts util
-    inputs.import-tree.url = "github:vic/import-tree"
+    inputs.import-tree.url = "github:vic/import-tree";
     
   };
 
