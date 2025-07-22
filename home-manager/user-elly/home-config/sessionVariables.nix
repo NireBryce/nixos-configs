c0054c8a7d970d7e5170a0a283176a2ce1012123
Nire Bryce <nire@computernope.net>
2025-07-22 18:31:08 -0400

    removed davinci resolve for now

--- home-manager/user-elly/home-config/sessionVariables.nix
+++ home-manager/user-elly/home-config/sessionVariables.nix
@@ -12,8 +12,8 @@
         LC_CTYPE                = "en_US.UTF-8";
         LS_COLORS               = "$(${pkgs.vivid}/bin/vivid generate dracula)";  # https://github.com/sharkdp/vivid
         EZA_COLORS              = "$(${pkgs.vivid}/bin/vivid generate dracula)";
-        STARSHIP_CONFIG         = "$HOME/.config/starship.toml";
-        STARSHIP_CACHE          = "$HOME/.cache/starship";
+        # STARSHIP_CONFIG         = "$HOME/.config/starship.toml";
+        # STARSHIP_CACHE          = "$HOME/.cache/starship";
         PYTHON                  = "PYTHON";
     };
 }
--- home-manager/user-elly/packages/video-editing/davinci-resolve.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, ... }:
-let
-    packageList = with pkgs; [
-        davinci-resolve
-    ];
-in 
-{
-    home.packages = packageList;
-}
