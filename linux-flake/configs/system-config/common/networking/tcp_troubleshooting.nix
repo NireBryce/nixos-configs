57c901fc55cc8b5fbd7a39bc2818a5cbf426fc03
Nire Bryce <nire@computernope.net>
2025-10-07 23:01:21 -0400

    fixed structure of tentative tcp fixes for planetside

--- linux-flake/configs/system-config/common/networking/tcp_troubleshooting.nix
+++ linux-flake/configs/system-config/common/networking/tcp_troubleshooting.nix
@@ -2,9 +2,10 @@
     ...
 }:
 {
-    networking.interfaces.wlp4s0.routes = [
+    networking.interfaces.wlp4s0.ipv4.routes = [
         {
             address = "0.0.0.0";
+            prefixLength = 0;
             via = "192.168.0.1";
             options.quickack = "1";
         }
