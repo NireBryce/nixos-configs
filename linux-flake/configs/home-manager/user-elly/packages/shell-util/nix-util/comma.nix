0d8a094b1c54a293158172f0254232f764aa168a
Nire Bryce <nire@computernope.net>
2025-12-01 22:10:56 -0500

    changed some things with nix path to make comma work

--- linux-flake/configs/home-manager/user-elly/packages/shell-util/nix-util/comma.nix
+++ linux-flake/configs/home-manager/user-elly/packages/shell-util/nix-util/comma.nix
@@ -1,10 +1,12 @@
 # TODO: fix comma
 # description = "`,` - `nix-shell -p` style run commands without installing to env";
 { pkgs, ... }:
-let packageList = with pkgs; [
-    comma
-];
+let 
+# packageList = with pkgs; [
+#     comma
+# ];
 in
 {
-    home.packages = packageList;
+    programs.nix-index-database.comma.enable = true;
+    # home.packages = packageList;
 }
