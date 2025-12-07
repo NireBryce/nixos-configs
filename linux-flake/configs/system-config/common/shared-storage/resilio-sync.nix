6ed7e04f907f2028b8ebbc530c8e04157218a479
Nire Bryce <nire@computernope.net>
2025-12-07 15:30:18 -0500

    updated NAS connection stuff

---
+++ linux-flake/configs/system-config/common/shared-storage/resilio-sync.nix
@@ -0,0 +1,15 @@
+{ ... }:
+let
+    o.enableWebUI = true;
+    o.storagePath = "/home/elly/.cache/resilio-sync";
+    o.directoryRoot = "/home/elly/Sync";
+in
+{
+    services.resilio = {
+        enable = true;
+        storagePath = o.storagePath;
+        enableWebUI = o.enableWebUI;
+        directoryRoot = o.directoryRoot;
+        
+    };
+}
