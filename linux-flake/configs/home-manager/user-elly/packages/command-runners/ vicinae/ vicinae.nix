844c02be4720a31483dfeb727e35bd317c777668
Nire Bryce <nire@computernope.net>
2025-12-16 18:37:10 -0500

    vicinae has a home manager service

--- linux-flake/configs/home-manager/user-elly/packages/command-runners/ vicinae/ vicinae.nix
+++ linux-flake/configs/home-manager/user-elly/packages/command-runners/ vicinae/ vicinae.nix
@@ -1,15 +1,29 @@
 { ... }:
 
 {
-    programs.vicinae = {
-        enable = true;
-        settings = {
+    # programs.vicinae = {
+    #     enable = true;
+    #     settings = {
             
+    #     };
+    #     systemd = {
+    #         enable = true;
+    #         autoStart = true;
+    #     };
+    services.vicinae = {
+        enable = true;
+        autoStart = true;
+        settings = {
+            faviconService = "twenty"; # twenty | google | none
+            font.size = 11;
+            popToRootOnClose = false;
+            rootSearch.searchFiles = false;
+            theme.name = "vicinae-dark";
+            window = {
+                csd = true;
+                opacity = 0.95;
+                rounding = 10;
+            };
         };
-        systemd = {
-            enable = true;
-            autoStart = true;
-        };
-
     };
 }
