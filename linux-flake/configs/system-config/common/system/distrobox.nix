3cc018b16bf2f236dc5fac898e3a2bb309d2baf7
Nire Bryce <nire@computernope.net>
2025-12-18 18:30:06 -0500

    distrobox

--- linux-flake/configs/system-config/common/system/distrobox.nix
+++ linux-flake/configs/system-config/common/system/distrobox.nix
@@ -5,7 +5,15 @@
         dockerCompat = true;
     };
     environment.systemPackages = with pkgs; [ 
-        distrobox 
+        distrobox
+        distroshelf
+        boxbuddy
     ];
+
+    # expose profile to distrobox containers
+    environment.etc."distrobox/distrobox.conf".text = ''
+        container_additional_volumes="/nix/store:/nix/store:ro /etc/profiles/per-user:/etc/profiles/per-user:ro /etc/static/profiles/per-user:/etc/static/profiles/per-user:ro"
+    '';
+
 }
 
