3ca1f5988bf16afedf712f35907c020ac7369b6b
Nire Bryce <nire@computernope.net>
2026-02-23 18:51:55 -0500

    clearing things that are no longer needed

--- linux-flake/configs/_refac/toplevel.nix
+++
@@ -1,4 +0,0 @@
-# nixosConfigurations are declared per-host via:
-#   flake.nixosConfigurations = inputs.self.lib.mkNixos "<system>" "<hostname>";
-# in each host's flake-parts module (e.g. hosts/nire-durandal/durandal-host-config.nix)
-{ }
