f7597e0cb10575f12f85b785a6d4a894c9e7ebaf
Nire Bryce <nire@computernope.net>
2026-02-25 17:48:26 -0500

    Revert "temporarily not including jovian"
    This reverts commit 8ef24bfbdcda069d056a221fc566ee437b68c996.

--- linux-flake/configs/system-config/wm-jovian/jovian-hhd.nix
+++ linux-flake/configs/system-config/wm-jovian/jovian-hhd.nix
@@ -1,37 +1,35 @@
-# {
-#     ...
-# }:
-# { den.aspects.jovian.nixos = 
-# { pkgs, config, ... }: 
-# {
-#     environment.systemPackages = with pkgs; [
-#         adjustor
-#     ];
+{
+    ...
+}:
+{ den.aspects.jovian.nixos = 
+{ pkgs, config, ... }: 
+{
+    environment.systemPackages = with pkgs; [
+        adjustor
+    ];
 
 
-#     # needed for tdp adjustor
-#     boot.extraModulePackages = [ config.boot.kernelPackages.acpi_call ];
+    # needed for tdp adjustor
+    boot.extraModulePackages = [ config.boot.kernelPackages.acpi_call ];
 
-#     services.handheld-daemon = {
-#     # if you're coming here from github search looking for ways to make HHD work,
-#     #     I need you to understand that the TDP control is currently mired in nixpkgs
-#     #     if you want it to work, it needs some work that I cannot do.
-#     #     https://github.com/NixOS/nixpkgs/pull/347279
-#         enable      = true;
-#         user        = "elly"; # TODO: use flake-parts to make this declared centrally
-#         ui.enable   = true;
-#         adjustor = {
-#             enable = true;
-#             loadAcpiCallModule = true;
-#         };
+    services.handheld-daemon = {
+    # if you're coming here from github search looking for ways to make HHD work,
+    #     I need you to understand that the TDP control is currently mired in nixpkgs
+    #     if you want it to work, it needs some work that I cannot do.
+    #     https://github.com/NixOS/nixpkgs/pull/347279
+        enable      = true;
+        user        = "elly"; # TODO: use flake-parts to make this declared centrally
+        ui.enable   = true;
+        adjustor = {
+            enable = true;
+            loadAcpiCallModule = true;
+        };
 
-#     };
+    };
 
-#     systemd.services."power-profiles-daemon" = {
-#         enable = false; # conflicts with adjustor in hhd
-#     };
+    systemd.services."power-profiles-daemon" = {
+        enable = false; # conflicts with adjustor in hhd
+    };
 
-# }
-# ;}
-
-# TODO: remove comments before starting nire-tenacity config
+}
+;}
