ecb9f8377cc3430b3d212e5442f8e8d52768a5ce
Nire Bryce <nire@computernope.net>
2026-03-19 17:27:58 -0400

    more debugging

--- _old/den.nix
+++ _old/den.nix
@@ -1,5 +1,6 @@
 # https://github.com/vic/vix/blob/unflake/modules/den.nix
 {
+    config,
     inputs,
     den,
     ...
@@ -8,7 +9,10 @@
     imports = [
         inputs.den.flakeModule
         # TODO: check this
-        # (inputs.den.namespace "elly" false)
+        (inputs.den.namespace "nire" true)
+        (inputs.den.namespace "packages" true)
+        (inputs.den.namespace "peripherals" true)
+        (inputs.den.namespace "elly" false)
     ];
 
     config._module.args.__findFile = den.lib.__findFile;
--- _old/dendritic/dendritic.nix
+++ _old/dendritic/dendritic.nix
@@ -13,9 +13,6 @@
 
 { inputs, lib, ... }:
 {
-    flake-file.inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-file.inputs.flake-file.url = lib.mkDefault "github:vic/flake-file";
-    flake-file.inputs.den.url = lib.mkDefault "github:vic/den";
     imports = [
         (inputs.flake-file.flakeModules.dendritic or { })
         (inputs.den.flakeModules.dendritic or { })
