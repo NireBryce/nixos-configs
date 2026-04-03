d3ea51159c23af766c707ee5af84351f368a44e8
Nire Bryce <nire@computernope.net>
2026-04-03 16:45:06 -0400

    consolodatin

--- linux-flake/modules/nire/font/font.nix
+++ linux-flake/modules/nire/font/font.nix
@@ -14,5 +14,18 @@
             useEmbeddedBitmaps = true;
         };
     };
+};
+
+flake.modules.homeManager.font = 
+{ pkgs, ... }:
+{
+    # font packages that are per-user through home-manager
+    # duplicated in system fonts, is this for non-nix hosts? consider moving to there
+    home.packages = with pkgs; [
+        nerd-fonts.fira-code
+        nerd-fonts.iosevka
+        nerd-fonts.jetbrains-mono
+    ];
 }
+
 ;}
--- linux-flake/modules/nire/shell-config/system-shell-config.nix
+++ linux-flake/modules/nire/shell-config/system-shell-config.nix
@@ -28,5 +28,47 @@
 
 
     
+};
+
+flake.modules.homeManager.shell-config = 
+{ pkgs, ... }:
+{
+    home.shellAliases = { 
+        # for in-place functions in aliases refer to:  https://stackoverflow.com/questions/34340575/zsh-alias-with-parameter
+        lcd             = ''f() { cd $1 && ls -lah };f'';               
+        cdls            = ''f() { cd $1 && ls -lah };f'';               
+        ll              = "ls -l";
+        cp              = "cp -i";    # Confirm before overwriting something
+        exa             = "${pkgs.eza}/bin/eza --icons=always"; # exa back compat for tools
+        # ls              = "${pkgs.eza}/bin/eza --icons=always --header --group-directories-first --hyperlink";
+        # gsa             = "${pkgs.git}/bin/git stash push";
+        img-cat         = "${pkgs.kitty}/bin/kitty +kitten icat";
+        kssh            = "${pkgs.kitty}/bin/kitty +kitten ssh";
+    };  
+    home.sessionVariables = { 
+        EDITOR                  = "micro";
+        MICRO_TRUECOLOR         = 1;
+        NIXPKGS_ALLOW_UNFREE    = 1;
+        PYTHONBREAKPOINT        = "ipdb.set_trace";
+        COLORTERM               = "truecolor";
+        PAGER                   = "less -R";
+        MANPAGER                = "${pkgs.bat}/bin/bat --language man";
+        LC_CTYPE                = "en_US.UTF-8";
+        LS_COLORS               = "$(${pkgs.vivid}/bin/vivid generate dracula)";  # https://github.com/sharkdp/vivid
+        EZA_COLORS              = "$(${pkgs.vivid}/bin/vivid generate dracula)";
+        # STARSHIP_CONFIG         = "$HOME/.config/starship.toml";
+        # STARSHIP_CACHE          = "$HOME/.cache/starship";
+        PYTHON                  = "PYTHON";
+    };
+    home.sessionPath = [ 
+        "/usr/local"
+        "/usr/bin"
+        "$HOME/bin"
+        "$HOME/.local/bin"
+        "$HOME/.nix-profile/bin"
+        "$HOME/.zi/bin"
+        "$HOME/.config/zi/bin"
+        "$HOME/.cargo/bin"
+    ];
 }
 ;}
