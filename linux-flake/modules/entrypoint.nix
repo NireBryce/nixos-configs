ae8ab2fb882bc0a891d3e23d9f82757327c9c222
Nire Bryce <nire@computernope.net>
2026-04-11 21:21:02 -0400

    ok, maybe extendLib needs to live on it's own

--- linux-flake/modules/entrypoint.nix
+++ linux-flake/modules/entrypoint.nix
@@ -1,7 +1,6 @@
 { 
     inputs,
     den, 
-    lib,
    ... 
 }:
 
@@ -54,24 +53,6 @@
             };
         };
     };
-
-    # Extends the lib module argument with all utility functions defined
-    # under modules/lib/. Any .nix file placed there is automatically
-    # picked up and its exported functions merged into lib.
-    # 
-    # must live above the lib directory
-
-    _module.args.lib = lib.extend (_: _:
-        lib.pipe (builtins.readDir ./lib) [
-            # Only pick up .nix files, ignore directories
-            (lib.filterAttrs (name: type: type == "regular" && lib.hasSuffix ".nix" name))
-            # Import each file, passing lib in case utilities need it
-            (lib.mapAttrs (name: _: import ./lib/${name} { inherit lib; }))
-            # Merge all the resulting attrsets into one
-            (lib.foldl' lib.mergeAttrs {})
-        ]
-    );
-
 
 
     # enables angle bracket syntax for imports shorthand
