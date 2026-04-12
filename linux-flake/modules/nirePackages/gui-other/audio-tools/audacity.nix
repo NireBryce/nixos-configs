da4b19259411a2d0d3b78557bf5d2af7b7123bc2
Nire Bryce <nire@computernope.net>
2026-04-12 02:20:56 -0400

    idk lol giving up for tonight

--- linux-flake/modules/nirePackages/gui-other/audio-tools/audacity.nix
+++ linux-flake/modules/nirePackages/gui-other/audio-tools/audacity.nix
@@ -1,9 +1,9 @@
-{ lib, ... }:
+{ pkgs, lib, ... }:
 let
   moduleName = lib.removeSuffix ".nix" (baseNameOf __curPos.file);
 in
 {
-  nire.moduleStore._.${moduleName}.homeManager = {
+  nire.moduleStore._.${moduleName}.homeManager = { ... }: {
     home.packages = with pkgs; [
       audacity
     ];
--- linux-flake/modules/nirePackages/gui-other/pkm/office/libre-office.nix
+++ linux-flake/modules/nirePackages/gui-other/pkm/office/libre-office.nix
@@ -3,7 +3,7 @@ let
   moduleName = lib.removeSuffix ".nix" (baseNameOf __curPos.file);
 in
 {
-  nire.moduleStore._.${moduleName}.homeManager = {
+  nire.moduleStore._.${moduleName}.homeManager = { pkgs, ... }: {
     # description = "libreoffice - office productivity software https://www.libreoffice.org/";
     home.packages = with pkgs; [
       libreoffice-qt
