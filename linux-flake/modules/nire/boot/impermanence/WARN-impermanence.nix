a855e16bb767956beac5f8e84875ad863e2e67d3
Nire Bryce <nire@computernope.net>
2026-04-16 17:19:04 -0400

    idk

--- linux-flake/modules/nire/boot/impermanence/WARN-impermanence.nix
+++ linux-flake/modules/nire/boot/impermanence/WARN-impermanence.nix
@@ -1,12 +1,12 @@
 { den, lib, ... }:
 let
   moduleName = lib.removeSuffix ".nix" (baseNameOf __curPos.file);
-  
+  aspectChain = "den.aspects.moduleStore._ = ${moduleName};"
 in
 {
  
    
-  den.aspects.moduleStore._.${moduleName} = den.lib.perHost {
+  ${aspectChain} = den.lib.perHost {
     nixos =
     { inputs, ... }:
     {
