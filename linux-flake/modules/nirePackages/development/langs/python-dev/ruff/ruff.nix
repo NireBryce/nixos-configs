ec2164e3171d270efd2323f1fd2103318bc87216
Nire Bryce <nire@computernope.net>
2026-05-29 12:12:17 -0400

    first change to a real package

--- linux-flake/modules/nirePackages/development/langs/python-dev/ruff/ruff.nix
+++ linux-flake/modules/nirePackages/development/langs/python-dev/ruff/ruff.nix
@@ -1,13 +1,12 @@
-{ lib, den, ... }:
+{ lib, pkgs, ... }:
 let
   moduleName = lib.removeSuffix ".nix" (baseNameOf __curPos.file);
 in
 {
-  den.aspects.moduleStore._.${moduleName} = den.lib.perUser { 
-        homeManager = { pkgs, ... }: {
-            home.packages = with pkgs; [
-                ruff
-            ];
-        };
+    flake.modules.home-manager.${moduleName} = {
+        home.packages = with pkgs; [
+            ruff
+        ];
     };
+    
 }
