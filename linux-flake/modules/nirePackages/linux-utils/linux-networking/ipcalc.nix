31034b5b636680dfa22a2bd99b103c3af78437de
Nire Bryce <nire@computernope.net>
2026-08-12 23:14:01 +0000

    Aug 12, 2026, 7:14 PM

--- linux-flake/modules/nirePackages/linux-utils/linux-networking/ipcalc.nix
+++ linux-flake/modules/nirePackages/linux-utils/linux-networking/ipcalc.nix
@@ -3,7 +3,7 @@
         moduleName = lib.removeSuffix ".nix" (baseNameOf __curPos.file);
     in {
         flake.modules.homeManager.${moduleName} = { pkgs, ... }: lib.mkIf (!pkgs.stdenv.isDarwin) {
-            # # description = "IP address calculator https://gitlab.com/ipcalc/ipcalc";
+            # # description = "IP address calculator https://gitlab.com/ipcalc/ipcalc";r
             home.packages = with pkgs; [
                 ipcalc
             ];
