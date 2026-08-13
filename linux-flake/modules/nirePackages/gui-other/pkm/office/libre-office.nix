e63fa37becb4876b6aa7d8a78a2e8e8b3010d639
Nire Bryce <nire@computernope.net>
2026-08-13 00:37:23 +0000

    Aug 12, 2026, 8:37 PM

--- linux-flake/modules/nirePackages/gui-other/pkm/office/libre-office.nix
+++ linux-flake/modules/nirePackages/gui-other/pkm/office/libre-office.nix
@@ -2,13 +2,11 @@
     let
         moduleName = lib.removeSuffix ".nix" (baseNameOf __curPos.file);
     in {
-        # nixpkgs' libreoffice-qt does not build for aarch64-darwin at all
-        # (pkgs.meta.platforms excludes it outright, same class of failure as
-        # the linux-utils/ packages -- see elly-home-manager.nix). On macOS the
-        # official app is what people actually use; add it as a homebrew cask
-        # in nire/macos/homebrew/homebrew.nix if it is wanted on nire-lysithea,
+        # meta.platforms excludes aarch64-darwin. On macOS the official app is
+        # what people actually use; add it as a homebrew cask in
+        # nire/macos/homebrew/homebrew.nix if it is wanted on nire-lysithea,
         # rather than trying to make this package work there.
-        flake.modules.homeManager.${moduleName} = { pkgs, lib, ... }: lib.mkIf (!pkgs.stdenv.isDarwin) {
+        flake.modules.homeManager.${moduleName} = { pkgs, ... }: {
             # # description = "libreoffice - office productivity software https://www.libreoffice.org/";
             home.packages = with pkgs; [
                 libreoffice-qt
