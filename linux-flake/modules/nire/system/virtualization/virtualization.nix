6038bda860dc06f5d6825dc9e199b07fd8b6d3a5
Nire Bryce <nire@computernope.net>
2026-08-14 00:45:28 -0400

    fixed commented out module part

--- linux-flake/modules/nire/system/virtualization/virtualization.nix
+++ linux-flake/modules/nire/system/virtualization/virtualization.nix
@@ -62,12 +62,12 @@
 
             };
 
-    flake.modules.homeManager.${moduleName} = { pkgs, ... }: {
-            # User-scoped `~/.config/containers/registries`
-            # https://wiki.nixos.org/wiki/Podman#DevContainers
-            # xdg.configFile."containers/registries.conf".text = ''
-            #   [registries.search]
-            #   registries = ['docker.io']
-            # '';
-        };
+    # flake.modules.homeManager.${moduleName} = { pkgs, ... }: {
+    #         User-scoped `~/.config/containers/registries`
+    #         https://wiki.nixos.org/wiki/Podman#DevContainers
+    #         xdg.configFile."containers/registries.conf".text = ''
+    #           [registries.search]
+    #           registries = ['docker.io']
+    #         '';
+    #     };
 }
